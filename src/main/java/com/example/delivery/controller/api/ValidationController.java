9fcd2f334f462490d0d1637487bd1e7762a226af
burning23185 <burning23@naver.com>
2024-03-16 21:21:05 +0900

    fix: vaildationController 중복체크 컨트롤러 수정

--- src/main/java/com/example/delivery/controller/api/ValidationController.java
+++ src/main/java/com/example/delivery/controller/api/ValidationController.java
@@ -1,13 +1,17 @@
-package com.example.delivery.controller;
+package com.example.delivery.controller.api;
 
 
+import com.example.delivery.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ValidationCotroller {
+@RequiredArgsConstructor
+public class ValidationController {
+    private final UserService userService;
 
     @PostMapping("/email-check")
     public ResponseEntity<Boolean> emailCheck(@RequestParam String email) {
