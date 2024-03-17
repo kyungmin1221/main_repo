a4be5de9ec48b7f8766165ba846218c88bbafe19
burning23185 <burning23@naver.com>
2024-03-17 16:42:46 +0900

    feat: store-main html 가게 메인 페이지 구현

--- src/main/java/com/example/delivery/controller/OrderController.java
+++ src/main/java/com/example/delivery/controller/OrderController.java
@@ -1,4 +1,14 @@
 package com.example.delivery.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class OrderController {
+    @GetMapping("/store-main")
+    public String registerStoreForm() {
+        return "store/store-main";
+    }
 }
